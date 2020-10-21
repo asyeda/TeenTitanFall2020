166662ca0d8ddd7a819f34c2d21125e0cefd3bb5
User <amathews4@ggc.edu>
2020-10-21 02:27:17 -0400

    made main methods and added println for each class

--- Elusive Game/src/Items.java
+++ Elusive Game/src/Items.java
@@ -1,4 +1,6 @@
 
 public class Items {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Items");
+	}
 }
--- Elusive Game/src/Room.java
+++ Elusive Game/src/Room.java
@@ -1,4 +1,5 @@
-
 public class Room {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Rooms");
+	}
 }
